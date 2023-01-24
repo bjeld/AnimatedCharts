81f31f9e913e8263c5178000ca17c8da843c1cee
bjeld <martinbjeld@gmail.com>
2023-01-24 11:47:50 +0100

    Initial Commit

---
+++ AnimatedCharts/AnimatedChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedChartsApp.swift
+//  AnimatedCharts
+//
+//  Created by Martin Bjeld on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
