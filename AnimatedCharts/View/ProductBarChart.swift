3796ad604f3f1c6180569d9154e98750da538ec9
bjeld <martinbjeld@gmail.com>
2023-09-08 19:39:20 +0200

    remove code not part of original tutorial

--- AnimatedCharts/View/ProductBarChart.swift
+++ AnimatedCharts/View/ProductBarChart.swift
@@ -5,8 +5,7 @@ struct ProductBarChart: View {
     var data: [Sales]
     
     var range: ClosedRange<Int>
-    var xrange: ClosedRange<Date> = Calendar.current.date(byAdding: .hour, value: -4, to: Date())!...Calendar.current.date(byAdding: .hour, value: 3, to: Date())!
-    @State var scale : CGFloat = 1.0
+    
     
     var body: some View {
         Chart {
@@ -26,13 +25,6 @@ struct ProductBarChart: View {
             }
         }
         .chartYScale(domain: range)
-        .chartXScale(domain: xrange)
-        .scaleEffect(scale)
-        .clipped()
-        .gesture(
-            MagnificationGesture().onChanged { value in
-            self.scale = value.magnitude
-        })
     }
 }
 
